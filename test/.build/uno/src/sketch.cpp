39c5ad2b14c2a9ae07f22fc8663d9ac083b55966
miokato@me.com <mio_k@mm.local>
2016-03-19 10:43:50 +0900

    add state machine behavior

--- test/.build/uno/src/sketch.cpp
+++ test/.build/uno/src/sketch.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
+void runMode(int state);
 void setup();
 void loop();
 int smoothFilter(int *buf, int raw, int _index);
@@ -8,39 +9,70 @@ int smoothFilter(int *buf, int raw, int _index);
 
 #define CDS 0
 #define BUF_LENGTH 10 
-#define THRESHOLD 70
+#define THRESHOLD 50
+#define HYSTERESIS 20
+
+// state
+#define FAR 0
+#define NEAR 1
 
 Servo myservo;
 
+
+void runMode(int state){
+  switch(state){
+    case FAR:
+      myservo.write(30);
+      break;
+    case NEAR:
+      myservo.write(120);
+      break;
+    default:
+      break;
+  }
+}
+
 void setup() {
   myservo.attach(9);
-  myservo.write(0);
+  myservo.write(30);
   Serial.begin(9600);
 }
 
 void loop() {
+  // sensor value buffer
   static int buffer[BUF_LENGTH];
   static int index = 0;
-  static boolean state = false;
+
+  // state
+  static int state = FAR;
+  static boolean wasNear = false;
+  boolean isNear = wasNear;
   
   int raw = analogRead(CDS);
   int smoothedValue = smoothFilter(buffer, raw, index);
 
-  if(smoothedValue<THRESHOLD){
-    myservo.write(180);
+  // state change if smooth value is less than THRESHOLD
+  if(smoothedValue > (THRESHOLD + HYSTERESIS)){
+    isNear = true;
+  } else if(smoothedValue < (THRESHOLD - HYSTERESIS)){
+    isNear = false;
   } else {
-    myservo.write(0);
-  }
+  }  
 
-  if(state) {
-    myservo.write(180);
+  if(!wasNear && isNear){
+    myservo.write(90);
+    Serial.println("state = near");
+  } else if(wasNear && !isNear){
+    myservo.write(30);
+    Serial.println("state = far");
   }
+  wasNear = isNear;
 
-  Serial.print("raw value is : ");
-  Serial.print(raw);
-  Serial.print('\t');
-  Serial.print("smoothed value is : ");
-  Serial.println(smoothedValue);
+  //Serial.print("raw value is : ");
+  //Serial.print(raw);
+  //Serial.print('\t');
+  //Serial.print("smoothed value is : ");
+  //Serial.println(smoothedValue);
   delay(6);
 }
 
@@ -53,7 +85,6 @@ int smoothFilter(int *buf, int raw, int _index){
   for (int i=0; i<BUF_LENGTH; i++){
     sum += buf[i];
   }
-
   return (int)(sum / BUF_LENGTH);
 }
 
--- test/src/sketch.ino
+++ test/src/sketch.ino
@@ -2,39 +2,51 @@
 
 #define CDS 0
 #define BUF_LENGTH 10 
-#define THRESHOLD 70
+#define THRESHOLD 50
+#define HYSTERESIS 20
 
 Servo myservo;
 
 void setup() {
   myservo.attach(9);
-  myservo.write(0);
+  myservo.write(30);
   Serial.begin(9600);
 }
 
 void loop() {
+  // sensor value buffer
   static int buffer[BUF_LENGTH];
   static int index = 0;
-  static boolean state = false;
+
+  // state
+  static boolean wasNear = false;
+  boolean isNear = wasNear;
   
   int raw = analogRead(CDS);
   int smoothedValue = smoothFilter(buffer, raw, index);
 
-  if(smoothedValue<THRESHOLD){
-    myservo.write(180);
+  // state change if smooth value is less than THRESHOLD
+  if(smoothedValue > (THRESHOLD + HYSTERESIS)){
+    isNear = true;
+  } else if(smoothedValue < (THRESHOLD - HYSTERESIS)){
+    isNear = false;
   } else {
-    myservo.write(0);
-  }
+  }  
 
-  if(state) {
-    myservo.write(180);
+  if(!wasNear && isNear){
+    myservo.write(90);
+    Serial.println("state = near");
+  } else if(wasNear && !isNear){
+    myservo.write(30);
+    Serial.println("state = far");
   }
+  wasNear = isNear;
 
-  Serial.print("raw value is : ");
-  Serial.print(raw);
-  Serial.print('\t');
-  Serial.print("smoothed value is : ");
-  Serial.println(smoothedValue);
+  //Serial.print("raw value is : ");
+  //Serial.print(raw);
+  //Serial.print('\t');
+  //Serial.print("smoothed value is : ");
+  //Serial.println(smoothedValue);
   delay(6);
 }
 
@@ -47,9 +59,5 @@ int smoothFilter(int *buf, int raw, int _index){
   for (int i=0; i<BUF_LENGTH; i++){
     sum += buf[i];
   }
-
   return (int)(sum / BUF_LENGTH);
 }
-
-
-
